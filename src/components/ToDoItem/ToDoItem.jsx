d0bdb04f391d7ab5b182ee3f32ad2ca3d11290b7
KyryloMuzychka <kyryll2016@gmail.com>
2024-08-13 17:23:54 +0300

    added ToDoItem

---
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styles from "./ToDoItem.module.css";
+import classNames from "classnames";
+
+import {
+  faTrash,
+  faAngleUp,
+  faAngleDown,
+  faCheck,
+  faExclamation,
+} from "@fortawesome/free-solid-svg-icons";
+import ManageButton from "../ManageButton/ManageButton";
+
+export default function ToDoItem({
+  task,
+  index,
+  deleteTask,
+  onToggleCompleted,
+  onToggleImportant,
+  moveTaskDown,
+  moveTaskUp,
+}) {
+  const itemStyle = classNames({
+    [styles.completed]: task.completed, // added if task.completed === true
+    [styles.important]: task.important, // added if task.important === true
+  });
+
+  return (
+    <div className={styles.item}>
+      <span className={itemStyle}>{task.title}</span>
+      <div className={styles.management}>
+        <ManageButton
+          typeButton={"delete_button"}
+          onClickButton={deleteTask}
+          icon={faTrash}
+          index={index}
+        />
+        <ManageButton
+          typeButton={"move_button"}
+          onClickButton={moveTaskUp}
+          icon={faAngleUp}
+        />
+        <ManageButton
+          typeButton={"move_button"}
+          onClickButton={moveTaskDown}
+          icon={faAngleDown}
+        />
+        <ManageButton
+          typeButton={"completed_button"}
+          onClickButton={onToggleCompleted}
+          icon={faCheck}
+        />
+        <ManageButton
+          typeButton={"important_button"}
+          onClickButton={onToggleImportant}
+          icon={faExclamation}
+        />
+      </div>
+    </div>
+  );
+}
